1c9fc4e7ec5cbfeb1191bdb791b1de01c1232470
Raushi007 <71937967+Raushi007@users.noreply.github.com>
2022-06-15 15:05:56 +0530

    Update grafana_annotations.groovy

--- grafana_annotations.groovy
+++ grafana_annotations.groovy
@@ -10,9 +10,9 @@ def annotateEventToGrafana(event) {
 
         cat << EOF > EventAnnotation.txt
 {
-  "text": "${event.capitalize()} commit ${COMMIT_ID} to '${STACKER_APP}' ${REGION} ${ENVIRONMENT} ${listener_type} Listener\\n\\n
-    <a href=\\"${GITHUB_URL}/commit/${LiveColorCommitId}\\">LiveListener: (${LiveColor}) (${LiveColorCommitId})</a>\\n
-    <a href=\\"${GITHUB_URL}/commit/${COMMIT_ID}\\">TestListener: (${testColor}) (${COMMIT_ID})</a>\\n
+  "text": "Deployed service 'backend-service'\n
+    <a href=\\"${GITHUB_URL}/commit/${LiveColorCommitId}\\">LiveListener: (${LiveColor}) (${LiveColorCommitId})</a>\n
+    <a href=\\"${GITHUB_URL}/commit/${COMMIT_ID}\\">TestListener: (${testColor}) (${COMMIT_ID})</a>\n
     <a href=\\"${BUILD_URL}/console\\">Jenkins #${BUILD_NUMBER} (DEPLOY Logs)</a>",
   "tags": [ "${event}", "env:${ENVIRONMENT}", "application:${STACKER_APP}", "commit:${COMMIT_ID}" ]
 }
